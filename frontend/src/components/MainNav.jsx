a456b37e98d953b99a081f8fddf5b46dab184b3c
murthehelp24 <attachaijumpahom@gmail.com>
2026-03-04 23:48:22 +0700

    update style css

--- frontend/src/components/MainNav.jsx
+++ frontend/src/components/MainNav.jsx
@@ -4,19 +4,19 @@ import styles from '../styles/Navbar.module.css'
 
 const MainNav = () => {
   return (
-    <nav className={`${styles.navContainer}`}>
+    <nav className={styles.navContainer}>
       <div className='mx-auto px-3'>
         <div className='flex justify-between h-16'>
-          <div className='flex  items-center gap-4'>
+          <div className='flex  items-center gap-3'>
             <Link to={'/'} className={styles.logo}>LOGO</Link>
             <Link to={'/'} className={styles.navLink}>Home</Link>
             <Link to={'/shop'} className={styles.navLink}>Shop</Link>
             <Link to={'/cart'} className={styles.navLink}>Cart</Link>
           </div>
 
-          <div className='flex items-center gap-4'>
-            <Link to={'/register'} className={styles.loginBtn}>Register</Link>
-            <Link to={'/login'}className={styles.loginBtn}>Login</Link>
+          <div className='flex items-center gap-3'>
+            <Link to={'/register'} className={styles.navLink}>Register</Link>
+            <Link to={'/login'}className={styles.navLink}>Login</Link>
           </div>
         </div>
       </div>
